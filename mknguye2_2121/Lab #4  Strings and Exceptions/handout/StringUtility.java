50f82e42a580a56d35121ea820e29fb6a05530eb
Kristopher <44072394+TD-QM@users.noreply.github.com>
2022-09-19 18:47:04 -0500

    Add files via upload

---
+++ mknguye2_2121/Lab #4  Strings and Exceptions/handout/StringUtility.java
@@ -0,0 +1,19 @@
+public class StringUtility {
+
+    public static String reverse(String sentence) {
+        // Delete the content in here and write your code in here.
+        return "";
+    }
+
+    public static char maxOccuringCharacter(String sentence) throws IllegalArgumentException {
+        // Delete this and write your code in here
+        return 'a';
+    }
+
+    public static boolean isPalindrome(String input) {
+        // Delete this and write your code in here
+
+        return result;
+
+    }
+}
