91dd19eba8e463736c2e966e0b8a6579926c8c41
MKNguye2 <MKNguye@uno.edu>
2023-09-20 08:37:16 -0500

    Phase 3+4 work

--- MKNguye2_4125/Phase 3/Main.java
+++ MKNguye2_4125/Phase 3/Main.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class Main{
     public static void main (String[] args){
         try {
-            FileWriter outfile = new FileWriter("phase1_results.csv"); // Object to write to the output file
-            File infile = new File("phase1_fall2023-1.txt"); // Object that acts as the input file
+
+            File infile = new File(String.format("%s.txt", args[0])); // Object that acts as the input file
             Scanner input = new Scanner(infile); // Object that reads the input file
+            FileWriter outfile = new FileWriter(String.format("%s.sql", args[0])); // Object to write to the output file
 
             // Loop until there's nothing left in the input file
             while(input.hasNextLine()){
@@ -24,19 +25,24 @@ public class Main{
 
                     // Go through the values array
                     for(String value: values){
+                        value = value.trim();
                         
                         try {   // I don't like the fact that the program only works if exceptions are thrown. Something just feels wrong about it
-                            Integer.parseInt(value.trim()); // Attempt to parse the value as an int
-                            output[index] = "Integer"; // If it succeeds, set the output's current index as Integer
+                            Integer.parseInt(value); // Attempt to parse the value as an int
+                            output[index] = value; // If it succeeds, set the output's current index as Integer
 
                         } catch (NumberFormatException e){
                             try {
-                                Float.parseFloat(value.trim()); // Attempt to parse the value as a float
-                                output[index] = "Float"; // If it succeeds, set the output's current index as Float
+                                Float.parseFloat(value); // Attempt to parse the value as a float
+                                output[index] = value; // If it succeeds, set the output's current index as Float
 
                             } catch(NumberFormatException e2){
-                                output[index] = "String"; // This is only called if both checks for the integer and float fail
-                                                          // Since we can assume that if it isn't an int or a float, it's a string, we don't need any checks
+                                if (value.compareTo("NULL") == 0){
+                                    output[index] = value;   // This is only called if both checks for the integer and float fail
+                                                                                // Since we can assume that if it isn't an int or a float, it's a string, we don't need any checks
+                                } else {
+                                    output[index] = "'" + value + "'";
+                                }
                             }
                         }
                         
@@ -44,7 +50,8 @@ public class Main{
                         index++; // Increment the index
                     }
 
-                    outfile.write(String.join(", ", output) + "\n"); // Concatenate the arrya and write it into the output file with a new line
+                    String sqlString = String.format("INSERT INTO %s VALUES(%s);\n", args[0], String.join(", ", output));
+                    outfile.write(sqlString); // Concatenate the arrya and write it into the output file with a new line
                 }
             }
 
@@ -53,7 +60,7 @@ public class Main{
             input.close();
 
         } catch (FileNotFoundException e){ // If the file isn't found (Assumed to be the output file which is bad code but if I had a check for both it would be too long)
-            File file = new File("./" + "phase1_results.csv");
+            File file = new File("./" + "phase3_results.txt");
 
             try{
                 file.createNewFile(); // If the file isn't found, just create it
---
+++ MKNguye2_4125/Phase 4/store_schema.sql
@@ -0,0 +1,55 @@
+DROP TABLE Review;
+DROP TABLE LineItem;
+DROP TABLE Address;
+DROP TABLE Order;
+DROP TABLE Product;
+DROP TABLE Customer;
+
+CREATE TABLE Customer (
+    C_ID CHAR,
+    C_Name VARCHAR(20),
+    C_DOB DATE,
+    C_Referrer CHAR,
+    CONSTRAINT CustomerPK PRIMARY KEY (C_ID),
+    CONSTRAINT C_CustomerFK FOREIGN KEY (C_Referrer) REFERENCES Customer(C_ID)
+);
+
+CREATE TABLE Product{
+    P_ID CHAR,
+    P_Name VARCHAR(20),
+    P_Price NUMBER(6,2),
+    CONSTRAINT ProductPK PRIMARY KEY (P_ID)
+};
+
+CREATE TABLE Order{
+    O_Number CHAR,
+    O_Date DATE,
+    O_Cust CHAR,
+    CONSTRAINT OrderPK PRIMARY KEY (O_Number),
+    CONSTRAINT O_CustomerFK FOREIGN KEY (O_Cust) REFERENCES Customer(C_ID)
+};
+
+CREATE TABLE Address{
+    A_Cust CHAR,
+    A_Addr CHAR,
+    CONSTRAINT AddressPK PRIMARY KEY (A_Cust, A_Addr),
+    CONSTRAINT A_CustomerFK FOREIGN KEY (A_Cust) REFERENCES Customer(C_ID)
+};
+
+CREATE TABLE LineItem{
+    L_Cust CHAR,
+    L_Product CHAR,
+    L_Quantity NUMBER,
+    CONSTRAINT LineItemPK PRIMARY KEY (L_Cust, L_Product),
+    CONSTRAINT L_CustomerFK FOREIGN KEY (L_Cust) REFERENCES Customer(C_ID),
+    CONSTRAINT L_ProductFK FOREIGN KEY (L_Product) REFERENCES Product(P_ID)
+};
+
+CREATE TABLE Review{
+    R_Product CHAR,
+    R_Number NUMBER,
+    R_Rating NUMBER(5),
+    R_Text CHAR,
+    CONSTRAINT ReviewPK PRIMARY KEY (R_Product, R_Number),
+    CONSTRAINT R_ProductFK FOREIGN KEY (R_Product) REFERENCES Product(P_ID)
+};
