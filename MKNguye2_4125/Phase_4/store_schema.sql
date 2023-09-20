df3fa3f488596fdfd392a95f13e72c23649295ca
MKNguye2 <MKNguye@uno.edu>
2023-09-20 15:13:56 -0500

    a

--- MKNguye2_4125/Phase_4/store_schema.sql
+++ MKNguye2_4125/Phase_4/store_schema.sql
@@ -6,39 +6,39 @@ DROP TABLE Product;
 DROP TABLE Customer;
 
 CREATE TABLE Customer (
-    C_ID CHAR,
+    C_ID CHAR(3),
     C_Name VARCHAR(20),
     C_DOB DATE,
-    C_Referrer CHAR,
+    C_Referrer CHAR(3),
     CONSTRAINT CustomerPK PRIMARY KEY (C_ID),
     CONSTRAINT C_CustomerFK FOREIGN KEY (C_Referrer) REFERENCES Customer(C_ID)
 );
 
 CREATE TABLE Product (
-    P_ID CHAR,
+    P_ID CHAR(3),
     P_Name VARCHAR(20),
     P_Price NUMBER(6,2),
     CONSTRAINT ProductPK PRIMARY KEY (P_ID)
 );
 
 CREATE TABLE Orders(
-    O_Number CHAR,
+    O_Number CHAR(5),
     O_Date DATE,
-    O_Cust CHAR,
+    O_Cust CHAR(3),
     CONSTRAINT OrderPK PRIMARY KEY (O_Number),
     CONSTRAINT O_CustomerFK FOREIGN KEY (O_Cust) REFERENCES Customer(C_ID)
 );
 
 CREATE TABLE Address(
-    A_Cust CHAR,
-    A_Addr CHAR,
+    A_Cust CHAR(3),
+    A_Addr VARCHAR2(30),
     CONSTRAINT AddressPK PRIMARY KEY (A_Cust, A_Addr),
     CONSTRAINT A_CustomerFK FOREIGN KEY (A_Cust) REFERENCES Customer(C_ID)
 );
 
 CREATE TABLE LineItem(
-    L_Cust CHAR,
-    L_Product CHAR,
+    L_Cust CHAR(3),
+    L_Product CHAR(3),
     L_Quantity NUMBER,
     CONSTRAINT LineItemPK PRIMARY KEY (L_Cust, L_Product),
     CONSTRAINT L_CustomerFK FOREIGN KEY (L_Cust) REFERENCES Customer(C_ID),
@@ -46,10 +46,10 @@ CREATE TABLE LineItem(
 );
 
 CREATE TABLE Review(
-    R_Product CHAR,
+    R_Product CHAR(3),
     R_Number NUMBER,
-    R_Rating NUMBER(5),
-    R_Text CHAR,
+    R_Rating NUMBER,
+    R_Text VARCHAR2(100),
     CONSTRAINT ReviewPK PRIMARY KEY (R_Product, R_Number),
     CONSTRAINT R_ProductFK FOREIGN KEY (R_Product) REFERENCES Product(P_ID)
 );
