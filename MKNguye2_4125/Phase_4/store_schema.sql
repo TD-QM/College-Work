5f37a1fba571c198647a1a303efe3e2d06ac9744
MKNguye2 <MKNguye@uno.edu>
2023-09-20 14:54:27 -0500

    aa

--- MKNguye2_4125/Phase_4/store_schema.sql
+++ MKNguye2_4125/Phase_4/store_schema.sql
@@ -14,42 +14,42 @@ CREATE TABLE Customer (
     CONSTRAINT C_CustomerFK FOREIGN KEY (C_Referrer) REFERENCES Customer(C_ID)
 );
 
-CREATE TABLE Product {
+CREATE TABLE Product (
     P_ID CHAR,
     P_Name VARCHAR(20),
     P_Price NUMBER(6,2),
     CONSTRAINT ProductPK PRIMARY KEY (P_ID)
-};
+);
 
-CREATE TABLE Order{
+CREATE TABLE Order(
     O_Number CHAR,
     O_Date DATE,
     O_Cust CHAR,
     CONSTRAINT OrderPK PRIMARY KEY (O_Number),
     CONSTRAINT O_CustomerFK FOREIGN KEY (O_Cust) REFERENCES Customer(C_ID)
-};
+);
 
-CREATE TABLE Address{
+CREATE TABLE Address(
     A_Cust CHAR,
     A_Addr CHAR,
     CONSTRAINT AddressPK PRIMARY KEY (A_Cust, A_Addr),
     CONSTRAINT A_CustomerFK FOREIGN KEY (A_Cust) REFERENCES Customer(C_ID)
-};
+);
 
-CREATE TABLE LineItem{
+CREATE TABLE LineItem(
     L_Cust CHAR,
     L_Product CHAR,
     L_Quantity NUMBER,
     CONSTRAINT LineItemPK PRIMARY KEY (L_Cust, L_Product),
     CONSTRAINT L_CustomerFK FOREIGN KEY (L_Cust) REFERENCES Customer(C_ID),
     CONSTRAINT L_ProductFK FOREIGN KEY (L_Product) REFERENCES Product(P_ID)
-};
+);
 
-CREATE TABLE Review{
+CREATE TABLE Review(
     R_Product CHAR,
     R_Number NUMBER,
     R_Rating NUMBER(5),
     R_Text CHAR,
     CONSTRAINT ReviewPK PRIMARY KEY (R_Product, R_Number),
     CONSTRAINT R_ProductFK FOREIGN KEY (R_Product) REFERENCES Product(P_ID)
-};
+);
