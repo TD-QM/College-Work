50101a327e191f5300ff87920e212143f3ab66ff
MKNguye2 <MKNguye@uno.edu>
2023-11-14 16:02:28 -0600

    update

--- MKNguye2_4125/Phase_9/Main.java
+++ MKNguye2_4125/Phase_9/Main.java
@@ -9,54 +9,64 @@ public class Main{
             Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Oracle21c", "MKNguye2", "TheFrenchiestFry678"); //Change these to your user/pass
 
             String invoiceString = "SELECT P_Name, L_Quantity, P_Price FROM Orders, LineItem, Product WHERE ? = O_Cust AND O_Number = L_Order AND L_Product = P_ID AND O_Date = ?";
-
             PreparedStatement invoiceStatement = conn.prepareStatement(invoiceString);
-
             invoiceStatement.setString( 1, args[0] );
             invoiceStatement.setString( 2, args[1] );
-
             ResultSet invoiceResults = invoiceStatement.executeQuery();
 
-            System.out.println("Product" + " ".repeat(24) + " Quantity  Price/Unit     Total");
-            System.out.println("-".repeat(63));
+            String customerName = "SELECT C_Name FROM Customer WHERE C_ID = ?";
+            PreparedStatement pStateName = conn.prepareStatement(customerName);
+            pStateName.setString(1, args[0]);
+            ResultSet customerNameResult = pStateName.executeQuery();
 
-            double orderTotal = 0;
-            while (invoiceResults.next()){
-                String productName = invoiceResults.getString(1);
-                int productDigits = productName.length();
-                System.out.print( productName + " ".repeat(31-productDigits) );
+            if(invoiceResults.next()){
+                invoiceResults.absolute(0);
 
-                int quantity = invoiceResults.getInt(2);
-                int quantityDigits = (int) Math.log10( quantity ) + 1;
-                System.out.print( " ".repeat( 8 - quantityDigits+1) + quantity + "  ");
+                System.out.println(args[0] + " invoice for " + customerNameResult.getString(1) + "\n");
+                System.out.println("Product" + " ".repeat(24) + " Quantity  Price/Unit     Total");
+                System.out.println("-".repeat(63));
 
-                double productPrice = invoiceResults.getDouble(3);
-                int priceDigits = (int) Math.log10( productPrice ) + 4;
-                System.out.print( "$" + " ".repeat(8 - priceDigits) + String.format("%.2f", productPrice) + "  ");
+                double orderTotal = 0;
+                while (invoiceResults.next()){
+                    String productName = invoiceResults.getString(1);
+                    int productDigits = productName.length();
+                    System.out.print( productName + " ".repeat(31-productDigits) );
 
-                double productTotal = productPrice*quantity;
-                int totalDigits = (int) Math.log10( productTotal ) + 4;
-                System.out.println("$" + " ".repeat(8 - totalDigits) + String.format("%.2f", productTotal));
+                    int quantity = invoiceResults.getInt(2);
+                    int quantityDigits = (int) Math.log10( quantity ) + 1;
+                    System.out.print( " ".repeat( 8 - quantityDigits+1) + quantity + "  ");
 
-                orderTotal += productTotal;
+                    double productPrice = invoiceResults.getDouble(3);
+                    int priceDigits = (int) Math.log10( productPrice ) + 4;
+                    System.out.print( "$" + " ".repeat(8 - priceDigits) + String.format("%.2f", productPrice) + "  ");
+
+                    double productTotal = productPrice*quantity;
+                    int totalDigits = (int) Math.log10( productTotal ) + 4;
+                    System.out.println("$" + " ".repeat(8 - totalDigits) + String.format("%.2f", productTotal));
+
+                    orderTotal += productTotal;
+                }
+
+                double shippingCost = 0;
+                double totalDue = orderTotal + shippingCost;
+
+                int orderTotalDigits = (int) Math.log10(orderTotal) + 4;
+                int shippingDigits = 4;
+                int totalDigits = (int) Math.log10(totalDue) + 4;
+
+                if (orderTotal < 35){
+                    shippingCost = 10;
+                    shippingDigits = 5;
+                }
+
+                System.out.println();
+                System.out.println("Total Product Cost :" + " ".repeat(8-orderTotalDigits) + "$" + String.format("%.2f", orderTotal));
+                System.out.println("Shipping Cost      :" + " ".repeat(8-shippingDigits) + "$" + String.format("%.2f", shippingCost));
+                System.out.println("Total Due          :" + " ".repeat(8-totalDigits) + "$" + String.format("%.2f", (orderTotal+shippingCost)));
+            } else {
+                
+                
             }
-
-            double shippingCost = 0;
-            double totalDue = orderTotal + shippingCost;
-
-            int orderTotalDigits = (int) Math.log10(orderTotal) + 4;
-            int shippingDigits = 4;
-            int totalDigits = (int) Math.log10(totalDue) + 4;
-
-            if (orderTotal < 35){
-                shippingCost = 10;
-                shippingDigits = 5;
-            }
-
-            System.out.println();
-            System.out.println("Total Product Cost :" + " ".repeat(8-orderTotalDigits) + "$" + String.format("%.2f", orderTotal));
-            System.out.println("Shipping Cost      :" + " ".repeat(8-shippingDigits) + "$" + String.format("%.2f", shippingCost));
-            System.out.println("Total Due          :" + " ".repeat(8-totalDigits) + "$" + String.format("%.2f", (orderTotal+shippingCost)));
 
             conn.close();
 
